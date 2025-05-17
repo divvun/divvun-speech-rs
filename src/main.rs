05b7b8e557a49fd43b503588e67318389c844ff0
Brendan Molloy <brendan@necessary.nu>
2025-05-17 15:26:39 +0200

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::sync::Arc;
+use std::{fmt::Display, sync::Arc};
 
-use divvun_speech::{Device, DivvunSpeech, SME_EXPANDED};
+use divvun_speech::{Device, DivvunSpeech, Options, ALL_SAMI, SME_EXPANDED};
 use tch::TchError;
 
 fn run() -> Result<(), TchError> {
@@ -9,7 +9,7 @@ fn run() -> Result<(), TchError> {
     // let m = multithread()?;
 
     // println!("{s:?} {m:?}");
-    println!("{s:?}");
+    // println!("{s:?}");
     Ok(())
 }
 
@@ -19,29 +19,41 @@ fn singlethread() -> Result<std::time::Duration, TchError> {
         // "/Users/brendan/Downloads/torchscript_sme_f.pt",
         // "/Users/brendan/git/divvun/divvun-speech-py/denoiser-jit.ptl",
         "/Users/brendan/git/divvun/divvun-speech-py/vocoder-jit.ptl",
-        SME_EXPANDED,
+        ALL_SAMI,
         Device::Cpu,
     )
     .unwrap();
 
     let start = std::time::Instant::now();
-    let text = "This is an example string";
+    let text = "Sami sami sami sami sami";
 
-    for _ in 0..100 {
-        let voice_data = match ds.forward(text, Default::default()) {
-            Ok(v) => v,
-            Err(e) => {
-                eprintln!("Error: {}", e);
-                return Err(e);
-            }
-        };
-        let _wav_data = DivvunSpeech::generate_wav(voice_data.copy()).unwrap();
+    for (lang, speakers) in SPEAKERS {
+        for speaker in *speakers {
+            println!("{} {}", lang, speaker);
+            let voice_data = ds.forward(text, Options { speaker: *speaker as i32, language: *lang as i32, pace: 1.0 })?;
+            let wav_data = DivvunSpeech::generate_wav(voice_data.copy())?;
+            std::fs::write(format!("./output/{lang}-{speaker}.wav"), wav_data).unwrap();
+        }
     }
+// ;
+//     let mut wav_data = vec![];
+//     for _ in 0..100 {
+//         let voice_data = match ds.forward(text, Options { speaker, language, pace: 1.05 }) {
+//             Ok(v) => v,
+//             Err(e) => {
+//                 eprintln!("Error: {}", e);
+//                 return Err(e);
+//             }
+//         };
+//         wav_data = DivvunSpeech::generate_wav(voice_data.copy()).unwrap();
+//     }
 
     let end = std::time::Instant::now();
 
     let diff = end - start;
-    println!("{:?}", diff);
+    // println!("Total: {:?}", diff);
+    // println!("Per: {:?} ms", (diff.as_millis() as f64 / 1000.0));
+    // std::fs::write("./output.wav", wav_data).unwrap();
     Ok(diff)
 }
 
@@ -97,3 +109,57 @@ fn main() {
         Err(e) => eprintln!("Error: {}", e),
     }
 }
+
+
+// languages = {"South Sámi":0,
+//           "North Sámi":1,
+//           "Lule Sámi":2}
+
+// speakers={#"aj0": 0,
+//           "Aanna - sma": 1,
+//           "acapela-male - sme": 2,
+//           "Siggá - smj": 3,
+//           "Biret - sme": 5,
+//           #"lo": 6,
+//           "ms - sme": 7,
+//           "Abmut - smj": 8,
+//           "Nihkol - smj": 9
+// }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(i32)]
+enum Lang {
+    SMA = 0,
+    SME,
+    SMJ,
+}
+
+impl Display for Lang {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[repr(i32)]
+enum Speaker {
+    Aanna = 1,
+    AcapelaMale = 2,
+    Siggá = 3,
+    Biret = 5,
+    Ms = 7,
+    Abmut = 8,
+    Nihkol = 9,
+}
+
+impl Display for Speaker {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+const SPEAKERS: &[(Lang, &[Speaker])] = &[
+    (Lang::SMA, &[Speaker::Aanna]),
+    (Lang::SME, &[Speaker::AcapelaMale, Speaker::Biret, Speaker::Ms]),
+    (Lang::SMJ, &[Speaker::Abmut, Speaker::Nihkol, Speaker::Siggá]),
+];
