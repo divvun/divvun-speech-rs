ea3baf88bbec24f9c9fd42c13ba7ef13c2bf7fe2
Brendan Molloy <brendan@necessary.nu>
2025-10-18 00:39:11 +0200

    I don't like linkers

--- build.rs
+++ build.rs
@@ -3,11 +3,4 @@ fn main() {
     if let Ok(path) = std::env::var("LIBTORCH") {
         println!("cargo:rustc-link-search=native={}/lib", path);
     }
-
-    println!("cargo:rustc-link-lib=c10");
-    println!("cargo:rustc-link-lib=torch");
-    println!("cargo:rustc-link-lib=torch_cpu");
-    if target.contains("ios") {
-        println!("cargo:rustc-link-lib=framework=Accelerate");
-    }
 }
