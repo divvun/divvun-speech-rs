a1165cd4e13aab23b0126f3c66c9033938a8a570
Brendan Molloy <brendan@necessary.nu>
2025-12-02 02:51:48 +0100

    more linking issues, oof

--- build.rs
+++ build.rs
@@ -2,11 +2,4 @@ fn main() {
     if let Ok(path) = std::env::var("LIBTORCH") {
         println!("cargo:rustc-link-search=native={}/lib", path);
     }
-
-    println!("cargo:rustc-link-lib=pthreadpool");
-    println!("cargo:rustc-link-lib=cpuinfo");
-    println!("cargo:rustc-link-lib=c10");
-    println!("cargo:rustc-link-lib=torch");
-    println!("cargo:rustc-link-lib=torch_cpu");
-    println!("cargo:rustc-link-lib=omp");
 }
